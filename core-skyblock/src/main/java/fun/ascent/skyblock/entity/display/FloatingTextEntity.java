f016b507f089d9bbb70d7b7fb50422c09909ee82
Cot_Itachi <protshanmainaly@gmail.com>
2026-05-02 07:22:31 +0545

    Combat System (the entity text display is a bit buggy)

--- core-skyblock/src/main/java/fun/ascent/skyblock/entity/display/FloatingTextEntity.java
+++ core-skyblock/src/main/java/fun/ascent/skyblock/entity/display/FloatingTextEntity.java
@@ -1,14 +1,15 @@
 package fun.ascent.skyblock.entity.display;
 
 import net.kyori.adventure.text.Component;
+import net.minestom.server.coordinate.Vec;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.LivingEntity;
 import net.minestom.server.entity.metadata.display.AbstractDisplayMeta;
 import net.minestom.server.entity.metadata.display.TextDisplayMeta;
 
 import java.util.function.Consumer;
 
-public class FloatingTextEntity extends LivingEntity {
+public class FloatingTextEntity extends Entity {
 
     public FloatingTextEntity(Component text, Consumer<TextDisplayMeta> setup) {
         super(EntityType.TEXT_DISPLAY);
@@ -19,6 +20,14 @@ public class FloatingTextEntity extends LivingEntity {
             meta.setHasNoGravity(true);
             meta.setSeeThrough(true);
             setup.accept(meta);
+        });
+    }
+
+    public void animateRise(float blocks, int durationTicks) {
+        editEntityMeta(TextDisplayMeta.class, meta -> {
+            meta.setTransformationInterpolationDuration(durationTicks);
+            meta.setTransformationInterpolationStartDelta(0);
+            meta.setTranslation(new Vec(0, blocks, 0));
         });
     }
 }
