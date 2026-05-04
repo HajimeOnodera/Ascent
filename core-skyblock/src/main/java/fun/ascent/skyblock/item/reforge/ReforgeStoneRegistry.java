9583d8904f39fb6f22555a3d7150cb7a7d919103
HajimeOnodera56 <hajime89121@gmail.com>
2026-05-05 01:00:13 +1200

    and this

--- core-skyblock/src/main/java/fun/ascent/skyblock/item/reforge/ReforgeStoneRegistry.java
+++ core-skyblock/src/main/java/fun/ascent/skyblock/item/reforge/ReforgeStoneRegistry.java
@@ -6,7 +6,7 @@ import fun.ascent.skyblock.item.reforge.types.BowReforge;
 
 public enum ReforgeStoneRegistry {
     PRECURSOR_GEAR(ArmorReforge.Ancient, new RarityStat(500, 750, 1000, 2000, 3500, 5000)),
-    OPTICAL_LENS(BowReforge.Precise,    new RarityStat(300, 500, 750, 1500, 2500, 4000));
+    OPTICAL_LENS(BowReforge.Precise, new RarityStat(300, 500, 750, 1500, 2500, 4000));
 
     private final Reforge reforge;
     private final RarityStat cost;
@@ -16,8 +16,8 @@ public enum ReforgeStoneRegistry {
         this.cost    = cost;
     }
 
-    public Reforge getReforge()           { return reforge; }
-    public int     getCost(Rarity rarity) { return (int) cost.fromRarity(rarity); }
+    public Reforge getReforge() { return reforge; }
+    public int getCost(Rarity rarity) { return (int) cost.fromRarity(rarity); }
 
     public static Reforge getReforgeForStone(String itemId) {
         for (ReforgeStoneRegistry entry : values()) {
