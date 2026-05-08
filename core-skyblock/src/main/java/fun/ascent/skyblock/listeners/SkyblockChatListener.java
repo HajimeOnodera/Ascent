d672981d9e56fdac60025911fd15355b7d153490
Whispering <ginafro240810@gmail.com>
2026-05-08 15:55:28 +0530

    fix: optimised chat logger

--- core-skyblock/src/main/java/fun/ascent/skyblock/listeners/SkyblockChatListener.java
+++ core-skyblock/src/main/java/fun/ascent/skyblock/listeners/SkyblockChatListener.java
@@ -14,9 +14,13 @@ import javax.net.ssl.HttpsURLConnection;
 import java.io.OutputStream;
 import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SkyblockChatListener {
 
+
+    public static List<String> messages = new ArrayList<>();
 
     public static String SB_CHAT_LOG ="https://discord.com/api/webhooks/1502192865424576603/C_nt5jP_HrRpBklB-Z1U8Q2rOZwLUkiKXDt6_HJFN_WrUaUXZssWOLhfp6GHq-3j32rg";
     public static void register() {
@@ -31,24 +35,37 @@ public class SkyblockChatListener {
                     .append(Component.text(event.getRawMessage()).color(messageColor));
             MinecraftServer.getConnectionManager().getOnlinePlayers().forEach(p -> p.sendMessage(displayName.append(message)));
 
-            String messageContent = "["+MinecraftServer.getServer().getPort() + "] " + event.getPlayer().getUsername() + ": " + event.getRawMessage();
+            String messageContent = "[" + MinecraftServer.getServer().getPort() + "] " +
+                    event.getPlayer().getUsername() + ": " + event.getRawMessage();
 
-            String jsonPayload = "{\"content\":\"" + messageContent + "\"}";
-            try {
-                URL url = URI.create(SB_CHAT_LOG).toURL();
-                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                conn.addRequestProperty("Content-Type", "application/json");
-                conn.addRequestProperty("User-Agent", "Java-Webhook");
-                conn.setDoOutput(true);
-                conn.setRequestMethod("POST");
-                OutputStream os = conn.getOutputStream();
-                os.write(jsonPayload.getBytes());
-                os.flush();
-                conn.getInputStream().close();
-                conn.disconnect();
-            } catch (Exception e) {
-                System.out.println("ERROR WHILE SENDING WEBHOOK, PLEASE CHECK");
-                System.out.println(e.getMessage());
+            messages.add(messageContent);
+            System.out.println("Put Message " + messages.size());
+            if(messages.size() >= 100){
+                System.out.println("Sending Messages");
+                StringBuilder msgBuilder = new StringBuilder();
+                messages.forEach((msg) -> {
+                    msgBuilder.append(msg);
+                    msgBuilder.append("\\n");
+                });
+                String jsonPayload = "{\"content\": \"" + msgBuilder + "\"}";
+                try {
+                    URL url = URI.create(SB_CHAT_LOG).toURL();
+                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                    conn.addRequestProperty("Content-Type", "application/json");
+                    conn.addRequestProperty("User-Agent", "Java-Webhook");
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod("POST");
+                    OutputStream os = conn.getOutputStream();
+                    os.write(jsonPayload.getBytes());
+                    os.flush();
+                    conn.getInputStream().close();
+                    conn.disconnect();
+                    messages.clear();
+                } catch (Exception e) {
+                    System.out.println("ERROR WHILE SENDING WEBHOOK, PLEASE CHECK");
+                    System.out.println(e.getMessage());
+                    System.out.println(jsonPayload);
+                }
             }
         });
     }
