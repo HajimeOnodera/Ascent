7de49e93bbb47644511069c22105a07e6b17dfa4
HajimeOnodera56 <hajime89121@gmail.com>
2026-05-14 20:34:29 +1200

    feat: Add ability handler, Rogue sword Done

--- core-skyblock/src/main/java/fun/ascent/skyblock/item/items/ItemDefinition.java
+++ core-skyblock/src/main/java/fun/ascent/skyblock/item/items/ItemDefinition.java
@@ -1,10 +1,18 @@
 package fun.ascent.skyblock.item.items;
 
+import fun.ascent.skyblock.item.ItemAbility;
 import fun.ascent.skyblock.item.SkyblockItem;
+import fun.ascent.skyblock.player.SkyblockPlayer;
 
 public interface ItemDefinition {
 
     String getItemId();
 
     SkyblockItem.Builder apply(SkyblockItem.Builder builder);
+
+    default ItemAbility ability() { return null; }
+
+    default void onRightClick(SkyblockPlayer player) {}
+
+    default void onLeftClick(SkyblockPlayer player) {}
 }
