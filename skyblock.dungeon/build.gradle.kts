16e6a75f0f67b33a7553af6f56fb75da4d92e8f9
HajimeOnodera56 <hajime89121@gmail.com>
2026-05-20 00:47:22 +1200

    fix: Dungeon Ligting issues

---
+++ skyblock.dungeon/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("io.github.goooler.shadow")
+}
+
+dependencies {
+    implementation(project(":commons"))
+    implementation(project(":database"))
+    implementation(project(":core-skyblock"))
+    implementation("net.kyori:adventure-text-minimessage:4.26.1")
+    implementation("net.minestom:minestom:2026.04.13-1.21.11")
+}
+
+tasks.shadowJar {
+    archiveFileName.set("SkyblockDungeon.jar")
+    manifest {
+        attributes["Main-Class"] = "fun.ascent.skyblock.dungeon.server.DungeonServer"
+    }
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
